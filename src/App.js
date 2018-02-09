ea47c31cf03837404076f7ce81c55b40414ca9f6
Dylan Espinosa <espinosadylan17@gmail.com>
2018-02-08 21:44:45 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,54 +1,9 @@
 import React, { Component } from "react";
 import { axios }from "axios";
 import Info from "./info";
-// import logo from './logo.svg';
 import "./App.css";
 import { Overlay } from "react-overlay";
 import Pics from "./picss";
-// import express from 'express';
-// // import request from 'request';
-// import morgan from 'morgan';
-
-// export class App extends Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       topspots: []
-//     }
-//   }
-//   componentWillMount() {
-//     axios
-//     .get('http://origin-top-spots-api.herokuapp.com/api/topspots')
-//     .then(response => response.data)
-//     .then(topspots => this.setState({ topspots }))
-
-//   }
-//   render(){
-//     return(
-//       <div className='card'>
-//         <div className= 'container bg-light'>
-//         <h1>San Diego Top Spots</h1>
-//         <p>A list of the top 30 places to see in San Diego, California.</p>
-//       </div>
-//       {/* <div className='container'> */}
-//       <div className='card'>
-//       {
-//     this.state.topspots.map(topspot => (
-//           <TopSpot
-//             key={topspot.id}
-//             name={topspot.name}
-//             description={topspot.description}
-//             location={topspot.location} />
-//     ))
-// }
-//       </div>
-//       </div>
-//     // </div>
-
-//   );
-//   }
-// }
 
 export class App extends Component {
   constructor(props) {
